99194e5eb1a331c1aa818f85aab26daa4d007a01
christ66 <schristou88@gmail.com>
2016-10-25 20:45:18 -0400

    Do not enable gitlab logo column by default.

--- src/main/java/org/jenkinsci/plugins/gitlablogo/GitlabLogoColumn.java
+++ src/main/java/org/jenkinsci/plugins/gitlablogo/GitlabLogoColumn.java
@@ -3,13 +3,19 @@ package org.jenkinsci.plugins.gitlablogo;
 import hudson.Extension;
 import hudson.model.Descriptor;
 import hudson.views.ListViewColumn;
+import hudson.views.ListViewColumnDescriptor;
 import net.sf.json.JSONObject;
 import org.kohsuke.stapler.StaplerRequest;
 
 public class GitlabLogoColumn extends ListViewColumn {
     @Extension
-    public static class GitlabLogoColumnDescriptor extends Descriptor<ListViewColumn>
+    public static class GitlabLogoColumnDescriptor extends ListViewColumnDescriptor
     {
+        @Override
+        public boolean shownByDefault() {
+            return false;
+        }
+
         @Override
         public String getDisplayName() {
             return "Gitlab Repository Icon";
